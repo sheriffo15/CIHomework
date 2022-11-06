8b409ea2e4780e7ab505accd6e216b0cdebfcd89
Nataliia Ilnytska <lelya_antonenko@bk.ru>
2022-11-06 17:34:27 +0300

    add test1

--- src/test/java/MaxServiceTest.java
+++ src/test/java/MaxServiceTest.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class MaxServiceTest {
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MaxServiceTest {
+
+    @Test
+    public void shouldFindMax() {
+        MaxService service = new MaxService();
+        int a = 5;
+        int b = 3;
+
+        int expected = a;
+        int actual = service.max(a, b);
+
+        Assertions.assertEquals(expected, actual);
+    }
 }
